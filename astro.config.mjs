44b9159f36b5bd54746ed07284a896fa7d04aeb6
Lyubomyr Rudko <rudkolg@gmail.com>
2026-04-23 21:23:24 +0300

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import partytown from '@astrojs/partytown';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import partytown from "@astrojs/partytown";
 
 export default defineConfig({
-  site: 'https://lyubomyr-rudko.github.io',
-  base: '/astro-blog-research',
+  site: "https://lyubomyr-rudko.github.io",
   integrations: [react(), partytown()],
 });
